e90400c294baa047e27c048fe8f59cfe40331191
Charlie Wu <charlie.y.wu@gmail.com>
2013-12-23 18:30:41 +1100

    Initial Commit

---
+++ WXKit/WXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WXAppDelegate.h
+//  WXKit
+//
+//  Created by Charlie Wu on 23/12/2013.
+//  Copyright (c) 2013 Charlie Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
