cf20c2fa40ee3fef0b582f93ee6bface423d6ba7
Charlie Wu <c.wu@webfm.net>
2014-03-27 09:49:16 +1100

    save context use overwrite

--- WXKit/WXKit/NSManagedObjectContext+WXKit.m
+++ WXKit/WXKit/NSManagedObjectContext+WXKit.m
@@ -8,13 +8,37 @@
 
 #import "NSManagedObjectContext+WXKit.h"
 
+
+
 @implementation NSManagedObjectContext (WXKit)
 
 - (void)saveContext
 {
-    NSError *error;
-    [self save:&error];
-    if (error)CLS_LOG(@"save context failed %@", error);
+    NSError *error = nil;
+    if ([self hasChanges] && ![self save:&error]) {
+        //        // Replace this implementation with code to handle the error appropriately.
+        //        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        //        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        //        abort();
+
+
+        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+
+        NSArray * conflictListArray = (NSArray*)[[error userInfo] objectForKey:@"conflictList"];
+        NSLog(@"conflict array: %@",conflictListArray);
+        NSError * conflictFixError = nil;
+
+        if ([conflictListArray count] > 0) {
+
+            NSMergePolicy *mergePolicy = [[NSMergePolicy alloc] initWithMergeType:NSOverwriteMergePolicyType];
+
+            if (![mergePolicy resolveConflicts:conflictListArray error:&conflictFixError]) {
+                NSLog(@"Unresolved conflict error %@, %@", conflictFixError, [conflictFixError userInfo]);
+                NSLog(@"abort");
+                abort();
+            }
+        }
+    }
 }
 
 @end
