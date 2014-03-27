ff96b0b8e1008bd4a08a2c9012e50a4eb080e52e
Charlie Wu <c.wu@webfm.net>
2014-03-27 11:22:53 +1100

    conflict resolution

--- WXKit/WXKit/NSManagedObjectContext+WXKit.m
+++ WXKit/WXKit/NSManagedObjectContext+WXKit.m
@@ -16,21 +16,10 @@
 {
     NSError *error = nil;
     if ([self hasChanges] && ![self save:&error]) {
-        //        // Replace this implementation with code to handle the error appropriately.
-        //        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        //        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        //        abort();
-
-
-        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-
         NSArray * conflictListArray = (NSArray*)[[error userInfo] objectForKey:@"conflictList"];
-        NSLog(@"conflict array: %@",conflictListArray);
         NSError * conflictFixError = nil;
-
         if ([conflictListArray count] > 0) {
-
-            NSMergePolicy *mergePolicy = [[NSMergePolicy alloc] initWithMergeType:NSOverwriteMergePolicyType];
+            NSMergePolicy *mergePolicy = [[NSMergePolicy alloc] initWithMergeType:NSRollbackMergePolicyType];
 
             if (![mergePolicy resolveConflicts:conflictListArray error:&conflictFixError]) {
                 NSLog(@"Unresolved conflict error %@, %@", conflictFixError, [conflictFixError userInfo]);
