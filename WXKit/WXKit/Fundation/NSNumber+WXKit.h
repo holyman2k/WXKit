4ac07aecb65ebbe9d42e0e30fa17ce9fb8aed361
Charlie Wu <c.wu@webfm.net>
2014-08-13 14:59:08 +1000

    refactor into folders and add nsnumber math methods

---
+++ WXKit/WXKit/Fundation/NSNumber+WXKit.h
@@ -0,0 +1,21 @@
+//
+//  NSNumber+WXKit.h
+//  WXKit
+//
+//  Created by Charlie Wu on 13/08/2014.
+//  Copyright (c) 2014 Charlie Wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNumber (WXKit)
+
+- (instancetype)addNumber:(NSNumber *)number;
+
+- (instancetype)multiplyNumber:(NSNumber *)number;
+
+- (instancetype)divideNumber:(NSNumber *)number;
+
+- (instancetype)dividedByNumber:(NSNumber *)number;
+
+@end
