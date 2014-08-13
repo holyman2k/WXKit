86a898c3c4c4aea929982510ea2262953931f465
Charlie Wu <c.wu@webfm.net>
2014-08-13 15:05:00 +1000

    fix merge bug

---
+++ WXKit/WXKit/WXKit.h
@@ -0,0 +1,37 @@
+//
+//  WXKit.h
+//  WXKit
+//
+//  Created by Charlie Wu on 23/12/2013.
+//  Copyright (c) 2013 Charlie Wu. All rights reserved.
+//
+
+#ifndef WXKit_WXKit_h
+#define WXKit_WXKit_h
+
+#import "Blocks.h"
+
+#import "Fundation/NSArray+WXKit.h"
+#import "Fundation/NSDate+WXKit.h"
+#import "Fundation/NSManagedObjectContext+WXKit.h"
+#import "Fundation/NSManagedObject+WXKit.h"
+#import "Fundation/NSMutableArray+WXKit.h"
+#import "Fundation/NSMutableString+WXKit.h"
+#import "Fundation/NSObject+WXKit.h"
+#import "Fundation/NSString+WXKit.h"
+#import "Fundation/NSURL+WXKit.h"
+
+#import "UIKit/UIColor+WXKit.h"
+#import "UIKit/UIDevice+WXKit.h"
+#import "UIKit/UIImage+WXKit.h"
+#import "UIKit/UIImageView+WXKit.h"
+#import "UIKit/UILabel+WXKit.h"
+#import "UIKit/UIView+WXKit.h"
+#import "UIKit/UIBarButtonItem+WXKit.h"
+
+#import "UIKitExtended/WXAlertView.h"
+#import "UIKitExtended/WXActionSheet.h"
+
+#import "Macros.h"
+
+#endif
