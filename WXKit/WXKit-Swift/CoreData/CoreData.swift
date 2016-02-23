32df619f69fbd3e41283f797093eb527b76d9121
Charlie Wu <c.wu@webfm.net>
2016-02-23 12:44:24 +1100

    improve core data code

--- WXKit/WXKit-Swift/CoreData/CoreData.swift
+++ WXKit/WXKit-Swift/CoreData/CoreData.swift
@@ -12,16 +12,45 @@ import CoreData
 
 // MARK: protocols
 
-protocol CoreDataKit {
+protocol ManagedObject {
 
     static var entityName:String {get};
 
     func deleteInContext(context:NSManagedObjectContext);
 }
 
+private protocol ManageObjectContext {
+
+}
+
 // MARK: protocol extensions
 
-extension CoreDataKit where Self : NSManagedObject {
+extension ManageObjectContext where Self : NSManagedObjectContext {
+
+    static func create(atUrl url:NSURL?, modelName:String, mergePolicy:NSMergePolicyType, options:[NSObject : AnyObject]?) -> Self? {
+
+        let storeType = url != nil ? NSSQLiteStoreType : NSInMemoryStoreType
+
+        if let modelUrl = NSBundle.mainBundle().URLForResource(modelName, withExtension: "momd"),
+            let managedObjectModel = NSManagedObjectModel(contentsOfURL: modelUrl) {
+                let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+                do {
+                    try persistentStoreCoordinator.addPersistentStoreWithType(storeType, configuration: nil, URL: url, options: options)
+                } catch   {
+                    abort()
+                }
+
+                let mergePolicy = NSMergePolicy(mergeType:  mergePolicy)
+                let context = Self(concurrencyType: .MainQueueConcurrencyType)
+                context.persistentStoreCoordinator = persistentStoreCoordinator
+                context.mergePolicy = mergePolicy
+                return context
+        }
+        return nil
+    }
+}
+
+extension ManagedObject where Self : NSManagedObject {
 
     static func createInContext(context:NSManagedObjectContext) -> Self? {
         var instance:Self? = nil
@@ -61,17 +90,24 @@ extension CoreDataKit where Self : NSManagedObject {
 
 // MARK: extensions
 
-private extension NSManagedObjectContext {
-    func fetch<T where T : NSManagedObject>(request : NSFetchRequest) -> [T] {
+extension NSManagedObjectContext : ManageObjectContext {
 
+    static func createWithModel(modelName:String,
+        storeName:String = "Database.sqlite",
+        mergePolicy:NSMergePolicyType = .MergeByPropertyObjectTrumpMergePolicyType,
+        options:[NSObject : AnyObject]? = [NSMigratePersistentStoresAutomaticallyOption:1, NSInferMappingModelAutomaticallyOption:1]) -> NSManagedObjectContext? {
+
+        let url = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).last!.URLByAppendingPathComponent(storeName)
+        return self.create(atUrl: url, modelName: modelName, mergePolicy: mergePolicy, options: options)
+    }
+
+    func fetch<T where T : NSManagedObject>(request : NSFetchRequest) -> [T] {
         let instances = try! self.executeFetchRequest(request)
         return (instances as? [T])!
-
     }
 }
 
-extension NSManagedObject : CoreDataKit {
-
+extension NSManagedObject : ManagedObject {
     static var entityName:String {
         return NSStringFromClass(self).componentsSeparatedByString(".").last! as String
     }
